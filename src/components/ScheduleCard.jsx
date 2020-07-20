50113879913ff36bd64cdea56173ee63c7d2f64d
Gina <ginakim.jh@gmail.com>
2020-07-19 22:24:44 -0700

    updated css for schedule card using sass

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 import { Card, Row, Col, Button, Divider } from "antd";
+import "../assets/css/ScheduleCard.scss";
 
 export default function ScheduleCard(props) {
     return (
-        <Card id="scheduleCard"
-            style={{
-                'borderRadius': '12px',
-                'borderStyle': 'solid',
-                'borderColor': 'rgb(165, 165, 165)',
-                'borderWidth': '1.5px'
-            }}>
+        <Card className="scheduleCard">
             <Row>
                 <Col span={4}>{props.date}</Col>
-                <Col span={2} className="colVerticalDivider">
-                    <Divider type="vertical" 
-                        style={{ 
-                            'height': '90%',
-                            'margin': '0 auto' }}/>
+                <Col span={2}>
+                    <Divider type="vertical" className="scheduleCardDivider"/>
                 </Col>
                 <Col span={18}>
                     <div>Designer: {props.designer}</div>
                     <div>Time: {props.timeStart}-{props.timeEnd}</div>
                     <div>{props.type}</div>
-                    <Button className="scheduleCardEditBtn"
-                        style={{
-                            'backgroundColor': 'rgb(36,36,36)',
-                            'color': 'white',
-                            'borderRadius': '10px'
-                        }}>Edit Schedule</Button>
+                    <Button className="scheduleCardEditBtn">Edit Schedule</Button>
                 </Col>
             </Row>
         </Card>
