f88d556ff75c559664c4569a0cc9e32b078b255e
Yongju Kwon <yongjukwon@Yongjus-MBP.vc.shawcable.net>
2020-07-22 15:40:52 -0700

    Create listing page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { Layout } from "antd";
-import { HashRouter as Router, Route } from "react-router-dom"
-import NavBar from "./components/NavBar"
-import Home from "./components/Home"
-import Listing from "./components/Listing"
-import Appointments from "./components/Appointments"
-import Account from "./components/Account"
+import { HashRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Listing from "./components/Listing";
+import Appointments from "./components/Appointments";
+import Account from "./components/Account";
 import "antd/dist/antd.css";
-import "./assets/css/App.css";
+import "./assets/scss/App.scss";
 
 const { Header, Content, Footer } = Layout;
 
 export default function App() {
-    return (
-        <Router>
-            <Layout className="layout">
-                <Header className="header">
-                    <NavBar />
-                </Header>
-                <Content className="outerContent">
-                    <div className="innerContent">
-                        <Route exact path="/" component={Home} />
-                        <Route path="/listing" component={Listing} />
-                        <Route path="/appointments" component={Appointments} />
-                        <Route path="/account" component={Account} />
-                    </div>
-                </Content>
-                <Footer className="footer" >LookUp WIP July 2020</Footer>
-            </Layout>
-        </Router>
-    );
+  return (
+    <Router>
+      <Layout className="layout">
+        <Header className="header">
+          <NavBar />
+        </Header>
+        <Content className="outerContent">
+          <div className="innerContent">
+            <Route exact path="/" component={Home} />
+            <Route path="/listing" component={Listing} />
+            <Route path="/appointments" component={Appointments} />
+            <Route path="/account" component={Account} />
+          </div>
+        </Content>
+        <Footer className="footer">LookUp WIP July 2020</Footer>
+      </Layout>
+    </Router>
+  );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,37 @@
 import React from "react";
 import { Menu, Dropdown, Button, Typography } from "antd";
-import { Link } from "react-router-dom"
-import "../assets/css/NavBar.css";
+import { Link } from "react-router-dom";
 
 const { Title } = Typography;
 
 export default function Navbar() {
-    const menu = (
-        <Menu>
-            <Menu.Item>
-                <Link to="/appointments">
-                    Appointments
-                </Link>
-            </Menu.Item>
-            <Menu.Divider />
-            <Menu.Item>
-                <Link to="/account">
-                    Account
-                </Link>
-            </Menu.Item>
-        </Menu>
-    );
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <Link to="/appointments">Appointments</Link>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item>
+        <Link to="/account">Account</Link>
+      </Menu.Item>
+    </Menu>
+  );
 
-    return (
-        <>
-            <div className="logo">
-                <Link to="/">
-                    <Title level={4}>LookUp</Title>
-                </Link>
-            </div>
-            <div className="listing">
-                <Link to="/listing">
-                    <Title level={4}>Listing</Title>
-                </Link>
-            </div>
-            <Dropdown className="profile" overlay={menu} placement="bottomRight">
-                <Button shape="round">Profile</Button>
-            </Dropdown>
-        </>
-    );
+  return (
+    <>
+      <div className="logo">
+        <Link to="/">
+          <Title level={4}>LookUp</Title>
+        </Link>
+      </div>
+      <div className="listing">
+        <Link to="/listing">
+          <Title level={4}>Listing</Title>
+        </Link>
+      </div>
+      <Dropdown className="profile" overlay={menu} placement="bottomRight">
+        <Button shape="round">Profile</Button>
+      </Dropdown>
+    </>
+  );
 }
