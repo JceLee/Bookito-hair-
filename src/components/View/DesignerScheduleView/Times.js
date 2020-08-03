09f87b7ef3c05a4ef7b722ec86694a07bbac8de3
erica <heejaerica@gmail.com>
2020-08-02 22:07:36 -0700

    Modify Service Dropdown Options

--- src/components/View/DesignerScheduleView/Times.js
+++ src/components/View/DesignerScheduleView/Times.js
@@ -3,16 +3,9 @@ import { Radio } from 'antd';
 import '../../../assets/scss/View/DesignerScheduleView/DesignerScheduleView.scss';
 
 export default function Times() {
-  //   const radioStyle = {
-  //     // display: 'block',
-  //     'margin-top': '20px',
-  //     'margin-right': '30px',
-  //     'border-radius': '10px',
-  //   };
-
   return (
     <div className='timeButtons'>
-      <Radio.Group defaultValue='time1' scroll={{ y: 10 }}>
+      <Radio.Group defaultValue='time1'>
         <Radio.Button
           value='time1'
           className='time1'
