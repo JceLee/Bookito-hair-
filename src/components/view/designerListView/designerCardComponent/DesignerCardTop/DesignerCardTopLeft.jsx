77a3be03b0e4ef91438a53d9549342539e3cad9d
Gina <ginakim.jh@gmail.com>
2020-08-04 16:07:25 -0700

    Update class name

--- src/components/view/designerListView/designerCardComponent/DesignerCardTop/DesignerCardTopLeft.jsx
+++ src/components/view/designerListView/designerCardComponent/DesignerCardTop/DesignerCardTopLeft.jsx
@@ -1,11 +1,11 @@
 import React from "../../../../../../node_modules/react";
-import { Col, Avatar } from "../../../../../../node_modules/antd";
+import { Row, Col, Avatar } from "../../../../../../node_modules/antd";
 import ReadOnlyStar from "../../../../commonComponents/ReadOnlyStar";
 import { UserOutlined} from '../../../../../../node_modules/@ant-design/icons';
 
 export default function DesignerCardTopLeft(props) {
   return (
-    <>
+    <Row className="designerCardTopLeft">
       <Col>
         <Avatar size={64} icon={<UserOutlined/>}/>
       </Col>
@@ -17,6 +17,6 @@ export default function DesignerCardTopLeft(props) {
           <ReadOnlyStar rating={props.rating}/>
         </div>
       </Col>
-    </>
+    </Row>
   );
 }
