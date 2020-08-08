d122c204f73ac149942d53e696ecccaaeb8ae27f
erica <heejaerica@gmail.com>
2020-08-07 18:48:24 -0700

    Merge MainView.jsx From dev to 5_Book_schedule_popup

--- src/components/view/mainView/MainView.jsx
+++ src/components/view/mainView/MainView.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import SearchBar from "../../commonComponents/searchBar/SearchBar";
-import "../../../assets/scss/view/mainView/MainView.scss";
+import React from 'react';
+import SearchBar from '../../commonComponents/searchBar/SearchBar';
+import '../../../assets/scss/view/mainView/MainView.scss';
 
 export default function MainView() {
-    return (
-        <>
-            <div className="searchBarSpace">
-                <SearchBar />
-            </div>
-            <div className="annoucementSpace">
-                annoucement space here
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className='searchBarSpace'>
+        <SearchBar />
+      </div>
+      <div className='annoucementSpace'>annoucement space here</div>
+    </>
+  );
 }
