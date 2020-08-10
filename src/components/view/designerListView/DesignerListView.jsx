13f27ad73f89195d1ab5f69d0e723039c16ef89b
JceLee <lkm4351@gmail.com>
2020-08-09 23:14:16 -0700

    Change the logic of hook

--- src/components/view/designerListView/DesignerListView.jsx
+++ src/components/view/designerListView/DesignerListView.jsx
@@ -8,30 +8,26 @@ import DesignerListFilter from "./DesignerListFilter";
 
 
 export default function DesignerListView(props) {
-
     const designers = useSelector((state) => state.firestore.designers);
     const dispatch = useDispatch();
-    const newDesigners = [];
-    const params = queryString.parse(props.location.search);
-    console.log("params:", params);
 
-
-    // Update API
-    firebaseDB
-    .firestore()
-    .collection("designers").where("Location", "==", "Vancouver")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.docs.forEach((doc) => {
-        newDesigners.push(doc.data());
-      });
-    });
-
-    // Line 24 is params from main page, Line 26, 27 are to dispatch action to reducer.
     useEffect(() => {
-    dispatch(load_database(newDesigners));
+        const params = queryString.parse(props.location.search);
+        const newDesigners = [];
+        console.log("params:", params);
+        firebaseDB
+            .firestore()
+            .collection("designers").where("Location", "==", "Vancouver")
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.docs.forEach((doc) => {
+                    newDesigners.push(doc.data());
+                });
+                dispatch(load_database(newDesigners));
+            });
+        console.log(designers);
     }, [dispatch]);
-    console.log(designers);
+
 
     // for testing
     const designer = {
