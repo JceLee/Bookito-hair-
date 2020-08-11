b0fbb8d85364ff8c5bb28f075298ca1c6972775e
Gina <ginakim.jh@gmail.com>
2020-08-11 16:23:30 -0700

    Change button type

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Card, Row, Col, Button, Divider } from "antd";
 
 export default function ScheduleCard(props) {
+    const { date, name, timeStart, timeEnd, types} = props;
     return (
         <Card className="scheduleCard"
             actions={[
-                <Button className="scheduleCardEditBtn">Edit Schedule</Button>
+                <Button type="text" className="scheduleCardEditBtn">Edit Schedule</Button>
             ]}>
             <Row>
-                <Col span={4} className="scheduleCardDate">{props.date}</Col>
+                <Col span={4} className="scheduleCardDate">{date}</Col>
                 <Col span={2}>
                     <Divider type="vertical" className="scheduleCardDivider"/>
                 </Col>
                 <Col span={18}>
-                    <div>Designer: {props.designer}</div>
-                    <div>Time: {props.timeStart}-{props.timeEnd}</div>
-                    <div>Type: {props.type}</div>
+                    <div>Designer: {name}</div>
+                    <div>Time: {timeStart}-{timeEnd}</div>
+                    <div>Type: {
+                        types.map((type, index) => 
+                            <span key={index}>{type} </span>
+                        )}</div>
                 </Col>
             </Row>
         </Card>
