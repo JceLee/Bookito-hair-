5cdd2b9dc783532b4dfec049fd3259f9f3747bd3
Gina <ginakim.jh@gmail.com>
2020-08-11 13:58:35 -0700

    Update scheduleCard css

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Card, Row, Col, Button, Divider } from "antd";
-import "../assets/css/ScheduleCard.scss";
 
 export default function ScheduleCard(props) {
     return (
-        <Card className="scheduleCard">
+        <Card className="scheduleCard"
+            actions={[
+                <Button className="scheduleCardEditBtn">Edit Schedule</Button>
+            ]}>
             <Row>
-                <Col span={4}>{props.date}</Col>
+                <Col span={4} className="scheduleCardDate">{props.date}</Col>
                 <Col span={2}>
                     <Divider type="vertical" className="scheduleCardDivider"/>
                 </Col>
                 <Col span={18}>
                     <div>Designer: {props.designer}</div>
                     <div>Time: {props.timeStart}-{props.timeEnd}</div>
-                    <div>{props.type}</div>
-                    <Button className="scheduleCardEditBtn">Edit Schedule</Button>
+                    <div>Type: {props.type}</div>
                 </Col>
             </Row>
         </Card>
