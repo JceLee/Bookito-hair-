9ec2e583c6d57dfd06b4b874542851ec984fef59
Gina <ginakim.jh@gmail.com>
2020-08-12 16:22:02 -0700

    Add tabs in clientScheduleView

--- src/components/view/clientScheduleView/ClientScheduleView.jsx
+++ src/components/view/clientScheduleView/ClientScheduleView.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 export default function ClientSchedule() {
-  return <div>Client Schedule Page</div>;
+
+    return (
+        <>
+            <div className="yourAppointments">Your Appointments</div>
+            <Tabs className="clientScheduleTabs" defaultActiveKey="1">
+                <TabPane className="upcomingTab" tab="Upcoming" key="1">
+                    Upcoming
+                </TabPane>
+                <TabPane className="pendingTab" tab="Pending" key="2">
+                    Pending
+                </TabPane>
+                <TabPane className="historyTab" tab="History" key="3">
+                    History
+                </TabPane>
+            </Tabs>
+        </>
+    );
 }
