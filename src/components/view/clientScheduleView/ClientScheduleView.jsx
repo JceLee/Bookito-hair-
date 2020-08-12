b63ba6537628a051ca95765ed097d3491011fa1b
Gina <ginakim.jh@gmail.com>
2020-08-12 16:54:49 -0700

    Remove testing imports

--- src/components/view/clientScheduleView/ClientScheduleView.jsx
+++ src/components/view/clientScheduleView/ClientScheduleView.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import ScheduleCardHistory from "../../commonComponents/ScheduleCardHistory";
 
 export default function ClientSchedule() {
-  return <div>Client Schedule Page
-      <ScheduleCardHistory types={["perm", "cut"]} />
-  </div>;
+  return <div>Client Schedule Page</div>;
 }
