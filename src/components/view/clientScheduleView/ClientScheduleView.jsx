3c9b81a8948c4afb490ae11bd2b9ad475dcfa2e8
Gina <ginakim.jh@gmail.com>
2020-08-12 17:09:22 -0700

    Adjust card size

--- src/components/view/clientScheduleView/ClientScheduleView.jsx
+++ src/components/view/clientScheduleView/ClientScheduleView.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Tabs } from 'antd';
+import ScheduleCard from "../../commonComponents/ScheduleCard";
+import ScheduleCardHistory from "../../commonComponents/ScheduleCardHistory";
 
 const { TabPane } = Tabs;
 
@@ -10,13 +12,15 @@ export default function ClientSchedule() {
             <div className="yourAppointments">Your Appointments</div>
             <Tabs className="clientScheduleTabs" defaultActiveKey="1">
                 <TabPane className="upcomingTab" tab="Upcoming" key="1">
-                    Upcoming
+                    <ScheduleCard name="Jane" date="Aug 12" timeStart="2:00" timeEnd="3:00" types={["cut", "perm"]}/>
                 </TabPane>
                 <TabPane className="pendingTab" tab="Pending" key="2">
-                    Pending
+                    <ScheduleCard name="John" date="Aug 12" timeStart="2:00" timeEnd="3:00" types={["cut", "perm"]}/>
+                    <ScheduleCard name="Jin" date="Aug 12" timeStart="2:00" timeEnd="3:00" types={["cut", "perm"]}/>
                 </TabPane>
                 <TabPane className="historyTab" tab="History" key="3">
-                    History
+                    <ScheduleCardHistory name="Jane" date="Aug 12" timeStart="2:00" timeEnd="3:00" types={["cut", "perm"]}/>
+
                 </TabPane>
             </Tabs>
         </>
