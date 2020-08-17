dc96049b3296ae9f4c14b1795f8e865fee7e0a72
Yongju Kwon <yongjukwon@Yongjus-MBP.vc.shawcable.net>
2020-08-16 23:43:59 -0700

    Remove duplicate codes in designerListView, DesignerTypeCarousel component

--- src/components/view/designerListView/DesignerListView.jsx
+++ src/components/view/designerListView/DesignerListView.jsx
@@ -7,52 +7,44 @@ import DesignerCardComponent from "./designerCardComponent/DesignerCardComponent
 import DesignerListFilter from "./DesignerListFilter";
 
 export default function DesignerListView(props) {
-    const designers = useSelector((state) => state.firestore.designers);
-    const dispatch = useDispatch();
+  const designers = useSelector((state) => state.firestore.designers);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const params = queryString.parse(props.location.search);
-        const newDesigners = [];
-        console.log("params:", params);
-        firebaseDB
-            .firestore()
-            .collection("designers").where("Location", "==", params["location"])
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.docs.forEach((doc) => {
-                    newDesigners.push(doc.data());
-                });
-                dispatch(load_database(newDesigners));
-            });
-    }, [dispatch]);
+  useEffect(() => {
+    const params = queryString.parse(props.location.search);
+    const newDesigners = [];
+    console.log("params:", params);
+    firebaseDB
+      .firestore()
+      .collection("designers")
+      .where("Location", "==", params["location"])
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.docs.forEach((doc) => {
+          newDesigners.push(doc.data());
+        });
+        dispatch(load_database(newDesigners));
+      });
+  }, [dispatch]);
 
-    return (
-        <>
-            <div className="listingContainer">
-                <div className="designerContainer">
-                    <div className="listNavBar">
-                        listNavBar
-                        <div className="filter">
-                            <DesignerListFilter numberOfDesigners='400' location='Vancouver' />
-                        </div>
-                    </div>
-                    {designers.map((designer, index) => (
-                        <div key = {index} className="designerList">
-                            <DesignerCardComponent designer={designer} />
-                        </div>
-                    ))}
-                </div>
-                <div className="mapContainer">
-                    Map container
-                </div>
+  return (
+    <>
+      <div className="listingContainer">
+        <div className="designerContainer">
+          <div className="listNavBar">
+            listNavBar
+            <div className="filter">
+              <DesignerListFilter
+                numberOfDesigners="400"
+                location="Vancouver"
+              />
             </div>
-            {/*<div className="sorter">*/}
-            {/*    <DesignerListFilter numberOfDesigners='400' location='Vancouver' />*/}
-            {/*</div>*/}
           </div>
-          <div className="designerList">
-            <DesignerCardComponent designer={designer} />
-          </div>
+          {designers.map((designer, index) => (
+            <div key={index} className="designerList">
+              <DesignerCardComponent designer={designer} />
+            </div>
+          ))}
         </div>
         <div className="mapContainer">Map container</div>
       </div>
