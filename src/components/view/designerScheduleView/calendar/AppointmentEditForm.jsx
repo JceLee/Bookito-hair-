c4883cfdb94b252961d1627bf9fcbf8ecd0c2b94
Gina <ginakim.jh@gmail.com>
2020-08-27 19:17:08 -0700

    Update appointment edit form

--- src/components/view/designerScheduleView/calendar/AppointmentEditForm.jsx
+++ src/components/view/designerScheduleView/calendar/AppointmentEditForm.jsx
@@ -6,23 +6,6 @@ export default function AppointmentEditForm(props) {
   const { appointmentData } = props;
   return (
     <>
-      {/* <AppointmentForm.BasicLayout
-        appointmentData={appointmentData}
-        onFieldChange={onFieldChange}
-        {...restProps}
-      >
-        {" "}
-        <AppointmentForm.Label text="Custom Field" type="title" />
-        <AppointmentForm.TextEditor
-          text="Type"
-          type="title"
-          value={appointmentData.customField}
-          onValueChange={onCustomFieldChange}
-        />
-      </AppointmentForm.BasicLayout> */}
-      {/* <AppointmentForm.Layout appointmentdata={appointmentData} {...restProps}>
-        Hi
-      </AppointmentForm.Layout> */}
       <div>{appointmentData.title}</div>
       <div>{appointmentData.startDate}</div>
       <div>{appointmentData.endDate}</div>
