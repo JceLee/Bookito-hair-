6658a1b299161d422a8ff3676fd97d4913f98daf
JceLee <lkm4351@gmail.com>
2020-09-26 00:57:50 -0700

    Merge remote-tracking branch dev into branch_24 & change folder structure & fix some bugs
    # Conflicts:
    #	src/assets/scss/view/designerScheduleView/DesignerScheduleView.scss
    #	src/components/view/designerProfileView/designerTop/DesignerTop.jsx
    #	src/components/view/designerScheduleView/DesignerScheduleView.jsx

--- src/components/view/designerScheduleView/DesignerScheduleView.jsx
+++ src/components/view/designerScheduleView/DesignerScheduleView.jsx
@@ -17,6 +17,20 @@ export default function DesignerSchedule() {
       timeEnd: "3:00",
       types: ["cut", "perm"],
     },
+    {
+      clientName: "bass",
+      date: "Aug 12",
+      timeStart: "5:00",
+      timeEnd: "6:00",
+      types: ["cut", "perm"],
+    },
+    {
+      clientName: "bass",
+      date: "Aug 13",
+      timeStart: "5:00",
+      timeEnd: "6:00",
+      types: ["cut", "perm"],
+    },
   ];
 
   const appointmentArray = [
@@ -30,8 +44,8 @@ export default function DesignerSchedule() {
     },
     {
       title: "Gina Kim",
-      startDate: "2020-09-02T09:45",
-      endDate: "2020-09-02T11:00",
+      startDate: "2020-09-04T09:45",
+      endDate: "2020-09-04T11:00",
       serviceName: "Men Haircut",
       price: 50,
       phoneNumber: "7781231234",
@@ -54,16 +68,16 @@ export default function DesignerSchedule() {
     },
     {
       title: "Gina Kim",
-      startDate: "2020-09-2309:45",
-      endDate: "2020-09-23T11:00",
+      startDate: "2020-09-2409:45",
+      endDate: "2020-09-24T11:00",
       serviceName: "Men Haircut",
       price: 50,
       phoneNumber: "7781231234",
     },
     {
       title: "Gina Kim",
-      startDate: "2020-09-30T09:45",
-      endDate: "2020-09-30T11:00",
+      startDate: "2020-09-17T09:45",
+      endDate: "2020-09-17T11:00",
       serviceName: "Men Haircut",
       price: 50,
       phoneNumber: "7781231234",
