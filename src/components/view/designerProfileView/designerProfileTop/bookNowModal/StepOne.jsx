15b20a576e66e91a8d816ce446687df500943028
erica <heejaerica@gmail.com>
2020-09-26 21:30:23 -0700

    Delete Unnecessary Div Classname

--- src/components/view/designerProfileView/designerProfileTop/bookNowModal/StepOne.jsx
+++ src/components/view/designerProfileView/designerProfileTop/bookNowModal/StepOne.jsx
@@ -4,33 +4,20 @@ import DayPicker from "react-day-picker";
 import "react-day-picker/lib/style.css";
 
 export default function StepOne(props) {
-  const {
-    timeSelection,
-    displayedDay,
-    handleDay,
-    radioChange,
-    bookingTime,
-  } = props;
+  const { timeSelection, displayedDay, handleDay, radioChange, bookingTime } = props;
 
   const renderCalendar = () => (
-    <div className="editDesignerCalendar">
-      <DayPicker
-        format={"MM/dd/yyyy"}
-        selectedDays={displayedDay}
-        onDayClick={handleDay}
-      />
-    </div>
+    <DayPicker format={"MM/dd/yyyy"} selectedDays={displayedDay} onDayClick={handleDay} />
   );
 
   const renderTime = () => (
-    <div className="timeButtons" id="selectTimePosition">
+    <div id="selectTimePosition">
       <Radio.Group>
         {timeSelection.map((hour, index) => {
           const { time, value, disabled } = hour;
           return (
             <Radio.Button
               key={index}
-              className="hourRadioBtn"
               value={time}
               disabled={disabled}
               checked={bookingTime === value}
@@ -45,13 +32,10 @@ export default function StepOne(props) {
   );
 
   return (
-    <div className="stepOne">
-      {/* <p id="title">Date and Time</p> */}
-      <Row>
-        <Col span={13}>{renderCalendar()}</Col>
-        <p id="selectTime">Please select time</p>
-        <Col span={11}>{renderTime()}</Col>
-      </Row>
+    <div>
+      {renderCalendar()}
+      <p id="selectTime">Please select time</p>
+      {renderTime()}
     </div>
   );
 }
