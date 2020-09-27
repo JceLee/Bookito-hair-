a2a0b60dbee8372dd6fea441fa67acaed9f2789b
erica <heejaerica@gmail.com>
2020-09-27 00:45:09 -0700

    Add Global Ant-Modal-Wrap Color as White

--- src/components/view/designerProfileView/designerProfileTop/bookNowModal/BookNowModal.jsx
+++ src/components/view/designerProfileView/designerProfileTop/bookNowModal/BookNowModal.jsx
@@ -285,7 +285,6 @@ export default function BookNowModal(props) {
         className="bookNowModal"
         title="Book Now"
         visible={visible}
-        width="100vw"
         footer={
           <div>
             {current > 0 && (
