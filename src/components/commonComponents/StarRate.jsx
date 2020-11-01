ad1daba6b71d9960a332bf9ac094cc015a875864
Joshua Shin <joshua_shin@hotmail.com>
2020-10-31 21:26:19 -0700

    Remove redundant inline styling

--- src/components/commonComponents/StarRate.jsx
+++ src/components/commonComponents/StarRate.jsx
@@ -18,9 +18,9 @@ export function StarRead(props) {
  
     return (
         <span className="starReadContainer">
-            <StarFilled className="starReadStar" style={{ fontSize: "14px", marginBottom: 0, marginTop: 3 }} />
-            <p className="starReadScore" style={{ fontWeight: 640, paddingLeft: 2, paddingRight: 2, marginBottom: 0 }}>{rateScore.toFixed(2)}</p>
-            <p className="starReadCount" style={{ marginBottom: 0 }}>{`(${rateCount})`}</p>
+            <StarFilled className="starReadStar" />
+            <p className="starReadScore">{rateScore.toFixed(2)}</p>
+            <p className="starReadCount">{`(${rateCount})`}</p>
         </span>
     );
 }
