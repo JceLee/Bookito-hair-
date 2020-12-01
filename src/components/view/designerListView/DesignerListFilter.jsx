42833c5d9082e3f9cb4df9aec283546eee7450cc
Joshua Shin <joshua_shin@hotmail.com>
2020-12-01 01:47:55 -0800

    Add designer list sort by UI only

--- src/components/view/designerListView/DesignerListFilter.jsx
+++ src/components/view/designerListView/DesignerListFilter.jsx
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { Menu, Dropdown, Button, DatePicker, TimePicker, Divider, Checkbox, Collapse } from "antd/lib/index";
-import { TagOutlined, CalendarOutlined, ClockCircleOutlined } from '@ant-design/icons';
-// <ControlOutlined /> <BarsOutlined />
-
-const { Panel } = Collapse;
+import { Menu, Dropdown, Button, DatePicker, TimePicker, Divider, Checkbox, Collapse, Select } from "antd/lib/index";
+import { TagOutlined, CalendarOutlined, ClockCircleOutlined, DownOutlined, CheckOutlined } from '@ant-design/icons';
 
 export default function DesignerListFilter(props) {
-  const { numberOfDesigners, location } = props;
-  
-  const [activeKey, setActiveKey] = useState([0]);
+  const { tags, numberOfDesigners, location } = props;
+  const { Panel } = Collapse;
 
-  const openFilterSetting = (i) => () => {
-    setActiveKey([i])
+  const [activeKey, setActiveKey] = useState([0]);
+  const [sortBy, setSortBy] = useState("distance");
+
+  const toggleFilterSetting = (i) => () => {
+    setActiveKey(activeKey[0] == i ? [0] : [i])
   };
 
+  function handleSortBy(e) {
+    setSortBy(e.key);
+  }
+
+  const sortMenu = (
+    <Menu onClick={handleSortBy}>
+      {/* <Menu.Item key="featured">Featured</Menu.Item> */}
+      <Menu.Item key="distance">{sortBy == "distance" && <CheckOutlined />}Distance</Menu.Item>
+      <Menu.Item key="review">{sortBy == "review" && <CheckOutlined />}Avg. Review</Menu.Item>
+      <Menu.Item key="priceLow">{sortBy == "priceLow" && <CheckOutlined />}Price Low</Menu.Item>
+      <Menu.Item key="priceHigh">{sortBy == "priceHigh" && <CheckOutlined />}Price High</Menu.Item>
+      <Menu.Item key="new">{sortBy == "new" && <CheckOutlined />}New</Menu.Item>
+    </Menu>
+  );
+
   const confirmFilterSetting = (
-    <Button className="designerListFilterConfirm" type="primary" block onClick={openFilterSetting(0)}>
+    <Button className="designerListFilterConfirm" type="primary" block onClick={toggleFilterSetting(0)}>
       Show results
     </Button>
   );
@@ -24,9 +38,14 @@ export default function DesignerListFilter(props) {
     <div className="designerListFilter">
       <Divider className="designerCardComponentDividerTop" />
         <div className="designerListFilterInner">
-          <Button className="filterBtn" onClick={openFilterSetting(1)}><span><TagOutlined /> Tags</span></Button>
-          <Button className="filterBtn" onClick={openFilterSetting(2)}><span><CalendarOutlined /> Date</span></Button>
-          <Button className="filterBtn" onClick={openFilterSetting(3)}><span><ClockCircleOutlined /> Time</span></Button>
+
+          <Dropdown overlay={sortMenu} trigger={["click"]} placement="bottomRight" onClick={toggleFilterSetting(0)}>
+            <Button className="filterBtn sortBtn">Sort<DownOutlined /></Button>
+          </Dropdown>
+
+          <Button className="filterBtn" onClick={toggleFilterSetting(1)}><span><TagOutlined /> Tag</span></Button>
+          <Button className="filterBtn" onClick={toggleFilterSetting(2)}><span><CalendarOutlined /> Date</span></Button>
+          <Button className="filterBtn" onClick={toggleFilterSetting(3)}><span><ClockCircleOutlined /> Time</span></Button>
           
           <Collapse ghost activeKey={activeKey}>
             <Panel className="flexChild" key="0" header={null} showArrow={false}>
