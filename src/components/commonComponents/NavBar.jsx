26e0a586ae858e08f8a9ce663b44421f746de6fc
jce2 <Jayce0817!>
2020-12-15 18:49:54 -0800

    first update for admin

--- src/components/commonComponents/NavBar.jsx
+++ src/components/commonComponents/NavBar.jsx
@@ -1,37 +1,53 @@
 import React from "react";
-import { Menu, Dropdown, Button, Typography, Drawer } from "antd";
-import { Link } from "react-router-dom";
-import { UserOutlined, BarsOutlined } from "@ant-design/icons";
+import {Menu, Dropdown, Button, Typography, Drawer} from "antd";
+import {Link} from "react-router-dom";
+import {UserOutlined, BarsOutlined} from "@ant-design/icons";
 import SignInModal from "../view/authView/SignInModal";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import SignOut from "../view/authView/SignOut";
 
 export default function Navbar() {
   const signedInUser = useSelector((state) => state.currentUser.currentUser);
 
-  const menuItems = signedInUser !== null ? [
-    { name: "Schedule (Designer)", link: "/designer_schedule" },
-    { name: "Schedule (Client)", link: "/client_schedule" },
-    { name: "Profile (Designer)", link: `/designer_profile?uid=${signedInUser.uid}` },
-    { name: "Profile (Client)", link: "/client_profile" },
-    { name: "Divider", link: "" },
-    { name: "Message", link: "/messenger" },
-    { name: "Log In", link: "" },
-  ] : [
-    { name: "Become a designer", link: "" },
-    { name: "Log In", link: "" },
-  ]
+  const menuItems = signedInUser === null ?
+    [
+      {name: "Become a designer", link: ""},
+      {name: "Log In", link: ""},
+    ]
+    : signedInUser.accountTypes === "admin" ?
+      [
+        {name: "Manage Accounts", link: "/administration"},
+        {name: "Log In", link: ""},
+      ]
+      : signedInUser.accountTypes === "client" ?
+        [
+          {name: "Schedule (Designer)", link: "/designer_schedule"},
+          {name: "Schedule (Client)", link: "/client_schedule"},
+          {name: "Profile (Designer)", link: `/designer_profile?uid=${signedInUser.uid}`},
+          {name: "Profile (Client)", link: "/client_profile"},
+          {name: "Divider", link: ""},
+          {name: "Message", link: "/messenger"},
+          {name: "Log In", link: ""},
+        ] : [
+          {name: "Schedule (Designer)", link: "/designer_schedule"},
+          {name: "Schedule (Client)", link: "/client_schedule"},
+          {name: "Profile (Designer)", link: `/designer_profile?uid=${signedInUser.uid}`},
+          {name: "Profile (Client)", link: "/client_profile"},
+          {name: "Divider", link: ""},
+          {name: "Message", link: "/messenger"},
+          {name: "Log In", link: ""},
+        ]
   ;
 
   const menu = (
     <Menu>
       {menuItems.map((menu, inx) => {
         if (menu.name == "Divider") {
-          return <Menu.Divider key={inx} />;
+          return <Menu.Divider key={inx}/>;
         } else if (menu.name == "Log In") {
           return (
             <Menu.Item key={inx}>
-              {signedInUser == null ? <SignInModal /> : <SignOut />}
+              {signedInUser == null ? <SignInModal/> : <SignOut/>}
             </Menu.Item>
           );
         } else {
@@ -52,8 +68,8 @@ export default function Navbar() {
 
       <div id="menuBtn">
         <Dropdown overlay={menu} placement="bottomRight" trigger={["click"]}>
-          <Button shape="round" icon={<UserOutlined />}>
-            <BarsOutlined />
+          <Button shape="round" icon={<UserOutlined/>}>
+            <BarsOutlined/>
           </Button>
         </Dropdown>
       </div>
