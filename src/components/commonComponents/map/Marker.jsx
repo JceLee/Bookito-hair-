b7dfe8aa0e9d24ea21cc9c7a74e23a623bc37073
Joshua Shin <joshua_shin@hotmail.com>
2021-01-05 01:20:06 -0800

    Make mobile designer listing map marker consistent to desktop

--- src/components/commonComponents/map/Marker.jsx
+++ src/components/commonComponents/map/Marker.jsx
@@ -41,40 +41,41 @@ export default function Marker(props) {
         setVisible(visible);
     };
 
+    const MarkerContent =
+        designer && <div className="markerContent">
+        <Link to={`/designer_profile?uid=${designer.uid}`}>
+            <Slider {...carouselSettings} className="markerCarousel">
+                {designer.works && designer.works.length ? designer.works.map((work, index) => (
+                    <div key={`galleryImg${index}`}><img src={work.url} alt={`Gallery img${index}`} /></div>
+                ))
+                : <div><img src={placeholder} alt={`Gallery placeholder`} /></div>}
+            </Slider>
+        </Link>
+        <div className="markerDescription">
+            <div className="markerDescriptionUpper">
+                <Link to={`/designer_profile?uid=${designer.uid}`}>
+                    <h4 className="markerDescriptionName">{designer.displayName}</h4>
+                </Link>
+                <StarRead rateScore={designer.rateScore || 0} rateCount={designer.rateCount || 0}/>
+                <p className="markerDescriptionDistance">{`${getDistanceFromLatLonInKm(lat, lng, userLocation && userLocation.lat, userLocation && userLocation.lng)}km from you`}</p>
+            </div>
+
+            <div className="markerDescriptionLower">
+                {designer.services && Object.keys(designer.services).map((serviceKey, index) => (
+                    designer.services[serviceKey] !== [] && 
+                    <Tag key={`serviceTag${index}`} className="serviceTag">{serviceKey}</Tag>
+                ))}
+            </div>
+        </div>
+    </div>
+
     return designer ?
         <div key={key} className="markerOuter">
 
             {/* Desktop version of marker using Popover */}
             {isDesktop ? <Popover 
                 overlayClassName="markerInner"
-                content={
-                    <div className="markerContent">
-                        <Link to={`/designer_profile?uid=${designer.uid}`}>
-                            <Slider {...carouselSettings} className="markerCarousel">
-                                {designer.works && designer.works.length ? designer.works.map((work, index) => (
-                                    <div key={`galleryImg${index}`}><img src={work.url} alt={`Gallery img${index}`} /></div>
-                                ))
-                                : <div><img src={placeholder} alt={`Gallery placeholder`} /></div>}
-                            </Slider>
-                        </Link>
-                        <div className="markerDescription">
-                            <div className="markerDescriptionUpper">
-                                <Link to={`/designer_profile?uid=${designer.uid}`}>
-                                    <h4 className="markerDescriptionName">{designer.displayName}</h4>
-                                </Link>
-                                <StarRead rateScore={designer.rateScore || 0} rateCount={designer.rateCount || 0}/>
-                                <p className="markerDescriptionDistance">{`${getDistanceFromLatLonInKm(lat, lng, userLocation && userLocation.lat, userLocation && userLocation.lng)}km from you`}</p>
-                            </div>
-
-                            <div className="markerDescriptionLower">
-                                {designer.services && Object.keys(designer.services).map((serviceKey, index) => (
-                                    designer.services[serviceKey] !== [] && 
-                                    <Tag key={`serviceTag${index}`} className="serviceTag">{serviceKey}</Tag>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                }
+                content={MarkerContent}
                 trigger="click"
                 visible={visible}
                 onVisibleChange={handleVisibleChange}
@@ -91,33 +92,11 @@ export default function Marker(props) {
                     footer={null}
                     onCancel={hide}
                     mask={false}
+                    width={380}
                     style={{top: "69.5vh"}} // Determines the location of the modal in mobile // TODO: THIS SHOULD BE FROM BOTTOM, BUT BOTTOM DOESNT WORK!!!
                     // maskClosable={false} // Determines whether clicking outside the modal closes the modal
                 >
-                        <div className="markerContent">
-                            <Link to={`/designer_profile?uid=${designer.uid}`}>
-                                <Slider {...carouselSettings} className="markerCarousel">
-                                    {designer.works && designer.works.length ? designer.works.map((imgSrc, index) => (
-                                        <div key={`galleryImg${index}`}><img src={imgSrc} alt={`Gallery img${index}`} /></div>
-                                    ))
-                                    : <div><img src={placeholder} alt={`Gallery placeholder`} /></div>}
-                                </Slider>
-                            </Link>
-                            <div className="markerDescription">
-                                <div className="markerDescriptionUpper">
-                                    <Link to={`/designer_profile?uid=${designer.uid}`}><h4 className="markerDescriptionName">{designer.displayName}</h4></Link>
-                                    <StarRead rateScore={designer.rateScore || 0} rateCount={designer.rateCount || 0}/>
-                                    <p className="markerDescriptionDistance">{`${getDistanceFromLatLonInKm(lat, lng, userLocation && userLocation.lat, userLocation && userLocation.lng)}km from you`}</p>
-                                </div>
-
-                                <div className="markerDescriptionLower">
-                                    {designer.services && Object.keys(designer.services).map((serviceKey, index) => (
-                                        designer.services[serviceKey] !== [] && 
-                                        <Tag key={`serviceTag${index}`} className="serviceTag" color="#332C1E">{serviceKey}</Tag>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
+                    {MarkerContent}
                 </Modal>
             </div>}
         </div>
